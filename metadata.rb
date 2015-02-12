e051d01c0c1907d766e1fdd118213b5a82e5cb56
David Baena <baena5999@gmail.com>
2015-02-12 22:15:48 +0100

    Adapted to chef solo development

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-repo'
+maintainer       'David Baena'
+maintainer_email 'baena5999@gmail.com'
+license          'All rights reserved'
+description      'Manage kini cloud infrastructure'
+
+version	'0.1.0'
